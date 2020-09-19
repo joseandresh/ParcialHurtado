b301db143abe2ea42aeb89c9cfc1b3329a1a10cd
joseandresh <a2019112027@estudiantes.upsa.edu.bo>
2020-09-19 11:45:48 -0400

    DataContext y Controller creado.

---
+++ ParcialHurtado/RestCountries/Controllers/CountriesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestCountries.Models;
+
+namespace RestCountries.Controllers
+{
+    public class CountriesController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Countries
+        public IQueryable<Country> GetCountries()
+        {
+            return db.Countries;
+        }
+
+        // GET: api/Countries/5
+        [ResponseType(typeof(Country))]
+        public IHttpActionResult GetCountry(string id)
+        {
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        // PUT: api/Countries/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCountry(string id, Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != country.alpha3Code)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Countries
+        [ResponseType(typeof(Country))]
+        public IHttpActionResult PostCountry(Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Countries.Add(country);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CountryExists(country.alpha3Code))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = country.alpha3Code }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [ResponseType(typeof(Country))]
+        public IHttpActionResult DeleteCountry(string id)
+        {
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            db.Countries.Remove(country);
+            db.SaveChanges();
+
+            return Ok(country);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CountryExists(string id)
+        {
+            return db.Countries.Count(e => e.alpha3Code == id) > 0;
+        }
+    }
+}
---
+++ ParcialHurtado/RestCountries/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace RestCountries.Models
+{
+	public class DataContext : DbContext
+	{
+		public DataContext() : base("DefaultConnection")
+		{
+
+		}
+
+		public System.Data.Entity.DbSet<RestCountries.Models.Country> Countries { get; set; }
+	}
+}
